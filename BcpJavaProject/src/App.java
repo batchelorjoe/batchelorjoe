92a1e50828ba05c05c6031b5edd111d00f313d26
Joe Batchelor <batchelorjoe@gmail.com>
2022-02-01 06:05:27 -0600

    Add Bcp class

--- BcpJavaProject/src/App.java
+++ BcpJavaProject/src/App.java
@@ -1,5 +1,9 @@
+import bcp.Bcp;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        new Bcp();
+        
     }
 }
